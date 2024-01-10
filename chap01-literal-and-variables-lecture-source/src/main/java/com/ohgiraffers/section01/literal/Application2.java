82dd82ae13a84e8dee8a40b0df5b9f6230c37f18
CJC0512 <ehdchs2006@gmail.com>
2024-01-10 14:30:12 +0900

    chap01 주석 방식 수정
    chap01 행 주석 방식에서 범위 주석 방식으로 변경

--- chap01-literal-and-variables-lecture-source/src/main/java/com/ohgiraffers/section01/literal/Application2.java
+++ chap01-literal-and-variables-lecture-source/src/main/java/com/ohgiraffers/section01/literal/Application2.java
@@ -3,14 +3,14 @@ package com.ohgiraffers.section01.literal;
 public class Application2 {
     public static void main(String[] args){
 
-        // 필기. 유용한 단축키
-        //  Ctrl + D: 복제
-        //  Ctrl(Alt) + Shift + 화살표: 해당 행 선택 후, 이동
+        /* 필기. 유용한 단축키
+        *       - Ctrl + D: 복제
+        *       - Ctrl(Alt) + Shift + 화살표: 해당 행 선택 후, 이동
+        * */
+        /* 수업목표. 값을 직접 연산하여 출력할 수 있다. */
 
-        // 수업목표. 값을 직접 연산하여 출력할 수 있다.
-
-        // 목차. 1. 숫자와 숫자의 연산
-        // 필기. 1-(1). % modulus 연산자(mod 연산자): 제수로 나눈 나머지. 즉, 나머지 연산
+        /* 목차. 1. 숫자와 숫자의 연산 */
+        /* 필기. 1-(1). % modulus 연산자(mod 연산자): 제수로 나눈 나머지. 즉, 나머지 연산 */
         System.out.println("========= 정수와 정수의 연산 =========");
         System.out.println(123 + 456);          // 579
         System.out.println(123 - 23);           // 100
@@ -18,7 +18,7 @@ public class Application2 {
         System.out.println(123 / 10);           // 12
         System.out.println(123 % 10);           // 3
 
-        // 필기. 1-(2). 실수는 정확하지 않은 근사값으로 저장되기에, 실수 연산에 대한 기대값과 실제 결과값이 다를 수 있다.
+        /* 필기. 1-(2). 실수는 정확하지 않은 근사값으로 저장되기에, 실수 연산에 대한 기대값과 실제 결과값이 다를 수 있다. */
         System.out.println("========= 실수와 실수의 연산 =========");
         System.out.println(1.23 + 1.23);        // 2.46
         System.out.println(1.23 - 0.23);        // 1.0
@@ -26,7 +26,7 @@ public class Application2 {
         System.out.println(1.23 / 10.0);        // 0.123
         System.out.println(1.23 % 1.0);         // 0.22999999999999998
 
-        // 필기. 1-(3). 정수와 실수 연산 시, 정수가 실수값으로 변환 된 후에 연산된다. 따라서, 결과값은 실수로 나온다.
+        /* 필기. 1-(3). 정수와 실수 연산 시, 정수가 실수값으로 변환 된 후에 연산된다. 따라서, 결과값은 실수로 나온다. */
         System.out.println("========= 정수와 실수의 연산 =========");
         System.out.println(123 + 0.5);          // 123.5
         System.out.println(123 - 0.5);          // 122.5
@@ -34,9 +34,9 @@ public class Application2 {
         System.out.println(123 / 0.5);          // 246.0
         System.out.println(123 % 0.5);          // 0.0
 
-        // 목차. 2. 문자의 연산
-        // 필기. 2-(1). 문자와 정수의 연산은 정수와 정수의 연산과 같다.(유니코드 기반의 숫자로 변환)
-        //  해석. 'a'는 아스키 코드로 97이기에, 정수 97 + 1 에 대한 연산이 되어 결과값 98이 나온다.
+        /* 목차. 2. 문자의 연산 */
+        /* 필기. 2-(1). 문자와 정수의 연산은 정수와 정수의 연산과 같다.(유니코드 기반의 숫자로 변환) */
+        /*  해석. 'a'는 아스키 코드로 97이기에, 정수 97 + 1 에 대한 연산이 되어 결과값 98이 나온다. */
         System.out.println("========= 문자와 정수의 연산 =========");
         System.out.println('a' + 1);            // 98
         System.out.println('a' - 1);            // 96
@@ -44,12 +44,12 @@ public class Application2 {
         System.out.println('a' / 2);            // 48
         System.out.println('a' % 2);            // 1
 
-        // 필기. 2-(2). 문자와 실수의 연산은 정수와 실수의 연산과 같다.
+        /* 필기. 2-(2). 문자와 실수의 연산은 정수와 실수의 연산과 같다. */
         System.out.println("========= 문자와 실수의 연산 =========");
         System.out.println('a' + 1.8);          // 98.8
 
-        // 목차. 3. 문자열 연산
-        // 필기. 3-(1). 문자열간의 연산은 덧셈(이어붙이기)만 가능하다.
+        /* 목차. 3. 문자열 연산 */
+        /* 필기. 3-(1). 문자열간의 연산은 덧셈(이어붙이기)만 가능하다. */
         System.out.println("========= 문자열과 문자열의 연산 =========");
         System.out.println("hello" + "world");  // helloworld
 //        System.out.println("hello" - "world");
@@ -57,8 +57,7 @@ public class Application2 {
 //        System.out.println("hello" / "world");
 //        System.out.println("hello" % "world");
 
-        // 필기. 3-(2). 문자열과 다른 형태의 값을 연산하면, 다른 형태의 값을 문자열로 변환해
-        //  문자열과 문자열의 연산으로 처리한다.
+        /* 필기. 3-(2). 문자열과 다른 형태의 값을 연산하면, 다른 형태의 값을 문자열로 변환해 문자열과 문자열의 연산으로 처리한다. */
         System.out.println("========= 문자열과 다른 형태의 값 연산 =========");
         // 설명. 문자열과 정수의 연산
         System.out.println("hellowworld" + 123);            // hellowworld123
--- chap01-literal-and-variables-lecture-source/src/main/java/com/ohgiraffers/section02/variable/Application1.java
+++ chap01-literal-and-variables-lecture-source/src/main/java/com/ohgiraffers/section02/variable/Application1.java
@@ -4,9 +4,72 @@ public class Application1 {
     public static void main(String[] args) {
         int num;
         num = 1;
-        int num2 = 0;
 
-        // 필기. soutv: 내가 선언한 변수를 선택하여 출력할 수 있도록 양식을 자동 작성해준다.
+
+        /* 필기. soutv: 내가 선언한 변수를 선택하여 출력할 수 있도록 양식을 자동 작성해준다. */
         System.out.println("num = " + num);
+
+        /* 수업목표. 변수의 사용 목적에 대해 이해할 수 있다.*/
+        /* 필기.
+        *   변수의 사용 목적
+        *   1. 값에 의미를 부여하기 위한 목적
+        *       - 의미 전달이 쉬워야 코드를 읽기 쉬워지고, 협업 및 유지보수에 유리하기 때문이다.
+        *   2. 한 번 저장해둔 값을 재사용하기 위한 목적
+        *       - 변수를 이용하여 코드를 작성하면, 값을 변경할 때도 보다 간편하게 변경할 수 있다.
+        *   3. 시간에 따라 변하는 값을 저장하고 사용할 수 있다.
+        *       - 변하는 값을 저장하기 위한 공간이다.
+        * */
+
+
+        /* 목차. 1. 값에 의미를 부여하기 위한 목적 */
+        System.out.println("=========== 값에 의미 부여 테스트 ===========");
+        System.out.println("보너스를 포함한 급여: " + (1000000 + 200000) + "원");
+
+        int salary = 1000000;
+        int bonus = 200000;
+        System.out.println("보너스를 포함한 급여: " + (salary + bonus) + "원");
+
+
+        /* 목차. 2. 한 번 저장해둔 값을 재사용하기 위한 목적 */
+        System.out.println("=========== 변수에 저장한 값 재사용 테스트 ===========");
+
+        /* 설명. 10명의 고객에게 100 포인트를 지급해주는 내용을 출력하도록 작성해보자. */
+        System.out.println("1번 고객에게 포인트를 100포인트 지급하였습니다.");
+        System.out.println("2번 고객에게 포인트를 100포인트 지급하였습니다.");
+        System.out.println("3번 고객에게 포인트를 100포인트 지급하였습니다.");
+        System.out.println("4번 고객에게 포인트를 100포인트 지급하였습니다.");
+        System.out.println("5번 고객에게 포인트를 100포인트 지급하였습니다.");
+        System.out.println("6번 고객에게 포인트를 100포인트 지급하였습니다.");
+        System.out.println("7번 고객에게 포인트를 100포인트 지급하였습니다.");
+        System.out.println("8번 고객에게 포인트를 100포인트 지급하였습니다.");
+        System.out.println("9번 고객에게 포인트를 100포인트 지급하였습니다.");
+        System.out.println("10번 고객에게 포인트를 100포인트 지급하였습니다.\n");
+
+        /* 설명. 위 코드에서 공통적으로 사용된 100이라고 하는 값을 변수에 넣고 변수를 호출하여 코드를 개선해 보자. */
+        int point = 100;
+        System.out.println("1번 고객에게 포인트를 " + point + " 지급하였습니다.");
+        System.out.println("2번 고객에게 포인트를 " + point + " 지급하였습니다.");
+        System.out.println("3번 고객에게 포인트를 " + point + " 지급하였습니다.");
+        System.out.println("4번 고객에게 포인트를 " + point + " 지급하였습니다.");
+        System.out.println("5번 고객에게 포인트를 " + point + " 지급하였습니다.");
+        System.out.println("6번 고객에게 포인트를 " + point + " 지급하였습니다.");
+        System.out.println("7번 고객에게 포인트를 " + point + " 지급하였습니다.");
+        System.out.println("8번 고객에게 포인트를 " + point + " 지급하였습니다.");
+        System.out.println("9번 고객에게 포인트를 " + point + " 지급하였습니다.");
+        System.out.println("10번 고객에게 포인트를 " + point + " 지급하였습니다.");
+
+
+        /* 목차. 3. 시간에 따라 변하는 값을 저장하고 사용할 목적 */
+        System.out.println("=========== 변수에 저장한 값 변경 테스트 ===========");
+        int sum = 0;
+
+        sum = sum + 10;     // 기존에 0이 담겨있던 변수에 10이 덮어씀(변수는 하나의 리터럴 값만 저장 가능)
+        System.out.println("sum에 10을 더하면 현재 sum의 값은 :" + sum);
+
+        sum = sum + 10;
+        sum = sum + 10;
+        sum = sum + 10;
+        sum = sum + 10;
+        System.out.println("sum에 10을 4번 더 누적하면 현재 sum의 값은 :" + sum);
     }
 }
