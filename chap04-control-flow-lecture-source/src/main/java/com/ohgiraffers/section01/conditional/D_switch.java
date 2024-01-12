c73a9d2900d9cde4feda83cf4929efeeae92ca8c
CJC0512 <ehdchs2006@gmail.com>
2024-01-12 17:51:32 +0900

    24.01.12 수업 완
    24.01.12 수업 완료로 인한 push

--- chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/D_switch.java
+++ chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/D_switch.java
@@ -8,18 +8,26 @@ public class D_switch {
         /* 수업목표. switch만 단독 사용 시 흐름을 이해하고 적용할 수 있다. */
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("본인의 등급을 입력하시오(G: 골드, 'S': 실버, 'B': 브론즈)  : ");
+        System.out.print("본인의 등급을 입력하시오(G: 골드, 'S': 실버, 'B': 브론즈)  : ");
         char grade = sc.next().charAt(0);
         int point = 0;
+        boolean flag = true;
 
         switch(grade) {
             case 'G': point += 10;
             case 'S': point += 10;
-            case 'B': point += 10;
+            case 'B':
+                point += 10;
+                break;
             default:
                 System.out.println("어디 회원 등급이세요?");
+                flag = false;
         }
 
-        System.out.println("당신의 등급은 " + grade + "이며, 현재 포인트는 " + point + " 입니다.");
+        if (flag){
+            System.out.println("당신의 등급은 " + grade + "이며, 현재 포인트는 " + point + " 입니다.");
+        } else  {
+            System.out.println("그럼 이참에 회원 가입 하시죠!");
+        }
     }
 }
---
+++ selfstudy/src/main/java/programers_selfstudy/selfstudy_practice1.java
@@ -0,0 +1,27 @@
+package programers_selfstudy;
+
+import java.util.Scanner;
+
+public class selfstudy_practice1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        int len = a.length();
+
+        String result = "";
+
+        for (int i=0; i < len; i++){
+            if (a.charAt(i) >= 'A' && a.charAt(i) <= 'Z'){
+                result += (char)(a.charAt(i) + 32);
+            }
+            else if (a.charAt(i) >= 'a' && a.charAt(i) <= 'z'){
+                result += (char)(a.charAt(i) - 32);
+            }
+            else{
+                continue;
+            }
+        }
+        System.out.print(result);
+    }
+}
