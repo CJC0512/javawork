e9a07c71ca91773f3afb128e649f39e72245342c
CJC0512 <ehdchs2006@gmail.com>
2024-01-16 11:53:07 +0900

    selfstudy_369 1차 첨삭 push
    selfstudy_369 1차 첨삭 push

--- selfstudy/src/main/java/array_lecture_source/selfstudy_369.java
+++ selfstudy/src/main/java/array_lecture_source/selfstudy_369.java
@@ -4,17 +4,17 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class selfstudy_369 {
-
-    static int i = 1;
+    static int num = 1;
     static boolean isWin = false;
     static boolean isPlay = true;
 
     public static void main(String[] args) {
+        /* 설명. 변수 초기화 */
         Scanner sc = new Scanner(System.in);
         String players_answer = null;
         int computers_condition = 0;
         String answer = null;
-        String nowI = new String();
+        String nowNum = null;
         boolean turn = false;       // turn = true 플레이어턴, turn = false 컴퓨터턴
         int stack = 0;
         System.out.println("369 게임에 오신것을 환영합니다.");
@@ -31,31 +31,30 @@ public class selfstudy_369 {
 
         /* 설명. 369게임 시작 */
         System.out.println("369 게임을 시작합니다...");
-        sc.nextLine();
+        sc.nextLine();                  // 개행 문자 제거
 
         do {
-            stack = 0;
-            nowI = Integer.toString(i); // i를 문자열로 변환
+            stack = 0;                  // stack 초기화
+            nowNum = Integer.toString(num);     // num을 문자열로 변환
+            stack = getStack(nowNum, stack);    // 현재 "짝"이 있는지 검사.
 
-            stack = getStack(nowI, stack);
             /* 설명. 현재 stack(짝의 개수)가 0 이라면 현재 i의 값을 반환*/
-            if (stack == 0) answer = nowI;
+            if (stack == 0) answer = nowNum;
             else answer = return_answer(stack);
 
-            if (turn) {  // 플레이어턴
+            if (turn) {                 // 플레이어 차례
                 System.out.print("\n 당신의 차례...\n당신의 답은?: ");
                 players_answer = sc.nextLine();
                 compare_answer(players_answer, answer);
 
                 turn = false;
-            } else {              // 컴퓨터턴
+            } else {                    // 컴퓨터 차례
                 System.out.print("\n컴퓨터의 차례...\n 컴퓨터의 답은?: ");
-                /* 설명. 약 5%의 확률로 컴퓨터가 오답을 반환 */
-                computers_condition = i + (int) Math.round(Math.random() * 0.1 + 0.405);
+                /* 설명. 컴퓨터의 상태. 약 5%의 확률로 컴퓨터가 오답을 반환 */
+                computers_condition = num + (int) Math.round(Math.random() * 0.1 + 0.405);
 
-
-                /* 설명. 컴퓨터의 condition에 따라 정답을 반환하거나 오답을 반환함.*/
-                if (computers_condition == i) {
+                /* 설명. 컴퓨터의 condition이 정상적이라면 정답을 반환. 비정상적이라면 오답을 반환 */
+                if (computers_condition == num) {
                     if (stack == 0) System.out.println(computers_condition);
                     else System.out.println(answer);
                     compare_answer(answer, answer);
@@ -66,9 +65,8 @@ public class selfstudy_369 {
                 }
                 turn = true;
             }
-
-            i++;
-            if (i > 100) isPlay = false;
+            num++;                    // num 증가
+            if (num > 100) isPlay = false;
         } while (isPlay);
 
         if (turn) System.out.println("플레이어 승리!!!");
@@ -76,6 +74,7 @@ public class selfstudy_369 {
     }
 
     private static int getStack(String nowI, int stack) {
+        /* 설명. 3, 6, 9가 있는 지 문자별로 검사 후, 그만큼 stack으로 "짝"의 개수를 반환*/
         for (int j = 0; j < nowI.length(); j++) {
             if (nowI.charAt(j) == '3' || nowI.charAt(j) == '6' || nowI.charAt(j) == '9') {
                 stack++;
@@ -89,7 +88,7 @@ public class selfstudy_369 {
             System.out.println("정답!!! " + answer);
 
         } else {
-            System.out.println("땡!!! 정답은 (" + i + ") " + answer);
+            System.out.println("땡!!! 정답은 (" + num + ") " + answer);
             isWin = true;
             isPlay = false;
         }
