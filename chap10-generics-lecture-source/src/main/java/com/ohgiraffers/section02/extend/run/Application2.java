573d77ccf8e2d80df122738e9f4053ef6796728c
CJC0512 <ehdchs2006@gmail.com>
2024-01-23 13:39:16 +0900

    chap10 section02 extend run app2 수정
    chap10 section02 extend run app2 수정

--- chap10-generics-lecture-source/src/main/java/com/ohgiraffers/section02/extend/run/Application2.java
+++ chap10-generics-lecture-source/src/main/java/com/ohgiraffers/section02/extend/run/Application2.java
@@ -7,25 +7,38 @@ public class Application2 {
 
         /* 수업목표. 와일드카드에 대해 이해할 수 있다. */
         WildCardFarm wildCardFarm = new WildCardFarm();
-        RabbitFarm<Rabbit> rabbitFarm = new RabbitFarm<>();
-        rabbitFarm.setAnimal(new Rabbit());
 
-        RabbitFarm<Bunny> bunnyRabbitFarm = new RabbitFarm<>(new Bunny());
-        RabbitFarm<DrunkenBunny> drunkenBunnyRabbitFarm = new RabbitFarm<>(new DrunkenBunny());
+        /* 필기. 방법-1 */
+//        RabbitFarm<Rabbit> rabbitFarm = new RabbitFarm<>();
+//        rabbitFarm.setAnimal(new Rabbit());
+//
+//        RabbitFarm<Bunny> bunnyRabbitFarm = new RabbitFarm<>(new Bunny());
+//        RabbitFarm<DrunkenBunny> drunkenBunnyRabbitFarm = new RabbitFarm<>(new DrunkenBunny());
+//
+//        wildCardFarm.anyType(rabbitFarm);
+//        wildCardFarm.anyType(bunnyRabbitFarm);
+//        wildCardFarm.anyType(drunkenBunnyRabbitFarm);
+//
+////        wildCardFarm.extendsType(rabbitFarm);
+//        wildCardFarm.extendsType(bunnyRabbitFarm);
+////        wildCardFarm.extendsType(drunkenBunnyRabbitFarm);
+//
+//        wildCardFarm.superType(rabbitFarm);
+//        wildCardFarm.superType(bunnyRabbitFarm);
+////        wildCardFarm.superType(drunkenBunnyRabbitFarm);
 
-        wildCardFarm.anyType(rabbitFarm);
-        wildCardFarm.anyType(bunnyRabbitFarm);
-        wildCardFarm.anyType(drunkenBunnyRabbitFarm);
 
-//        wildCardFarm.extendsType(rabbitFarm);
-        wildCardFarm.extendsType(bunnyRabbitFarm);
-//        wildCardFarm.extendsType(drunkenBunnyRabbitFarm);
+        /* 필기. 방법-2 */
+        wildCardFarm.anyType(new RabbitFarm<Rabbit>(new Rabbit()));
+        wildCardFarm.anyType(new RabbitFarm<Bunny>(new Bunny()));
+        wildCardFarm.anyType(new RabbitFarm<DrunkenBunny>(new DrunkenBunny()));
 
-        wildCardFarm.superType(rabbitFarm);
-        wildCardFarm.superType(bunnyRabbitFarm);
-//        wildCardFarm.superType(drunkenBunnyRabbitFarm);
+//        wildCardFarm.extendsType(new RabbitFarm<Rabbit>(new Rabbit()));
+        wildCardFarm.extendsType(new RabbitFarm<Bunny>(new Bunny()));
+//        wildCardFarm.extendsType(new RabbitFarm<DrunkenBunny>(new DrunkenBunny()));
 
-
-
+        wildCardFarm.superType(new RabbitFarm<Rabbit>(new Rabbit()));
+        wildCardFarm.superType(new RabbitFarm<Bunny>(new Bunny()));
+//        wildCardFarm.superType(new RabbitFarm<DrunkenBunny>(new DrunkenBunny()));
     }
 }
