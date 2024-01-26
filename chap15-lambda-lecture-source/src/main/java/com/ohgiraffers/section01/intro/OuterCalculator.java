7049138964d003f535d89608d33274d0cb67b249
CJC0512 <ehdchs2006@gmail.com>
2024-01-26 14:07:21 +0900

    chap15 lambda 작성 및 push

---
+++ chap15-lambda-lecture-source/src/main/java/com/ohgiraffers/section01/intro/OuterCalculator.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.section01.intro;
+
+public interface OuterCalculator {
+    @FunctionalInterface
+    public interface Sum {
+        int sumTwoNumber(int a, int b);
+    }
+    @FunctionalInterface
+    public interface Minus {
+        int minusTwoNumber(int a, int b);
+    }
+    @FunctionalInterface
+    public interface Multiply {
+        int multiplyTwoNumber(int a, int b);
+    }
+    @FunctionalInterface
+    public interface Divide {
+        int divideTwoNumber(int a, int b);
+    }
+}
