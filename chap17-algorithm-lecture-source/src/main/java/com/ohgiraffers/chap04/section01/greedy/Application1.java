349da5174e13ae10b745b9f5b6496adb49faae07
CJC0512 <ehdchs2006@gmail.com>
2024-02-01 12:18:29 +0900

    chap17 greedy, tree push

---
+++ chap17-algorithm-lecture-source/src/main/java/com/ohgiraffers/chap04/section01/greedy/Application1.java
@@ -0,0 +1,43 @@
+package com.ohgiraffers.chap04.section01.greedy;
+
+public class Application1 {
+    public static int solution(int n) {
+        int count = 0;      // 들고 갈 최소 봉지 갯수
+
+        while (true) {
+
+            /* 설명. 5키로로 바로 나눌 수 있으면 n/5를 반환한다. */
+            if (n % 5 == 0) {
+                return n / 5 + count;       // + count 의 개념은 이전 반복에서 가져간 3킬로그램 봉지의 수이다.
+            } else if (n < 0) {
+                return -1;
+            }
+
+            /* 설명. 3킬로그램 봉지로 한 봉지 들고 간다. */
+            n = n - 3;
+            count++;
+        }
+//        return count;
+    }
+
+    /* 설명. 나의 soluntion. 5와 3을 굳이 명시해주어, 나중에 변수(5,3)가 변하더라도 반영하여 사용할 수 있도록 하였다. */
+    public static int mySolution(int n) {
+        int count = -1;      // 들고 갈 최소 봉지 갯수. 기본값은 오류로 처리한다.
+        int num1 = 5;
+        int num2 = 3;
+        int divN = n % (num1 * num2);       // 5와 3의 공배수로 나눠지고 난 후 나머지
+        int num1Count = (n / (num1 * num2)) * num1;           // 사용되는 5의 개수. 이해하기 쉽도록 굳이 풀어서 작성하였다.
+
+        /*  설명. 나머지 수가 3으로 나눠질 수 있는지 확인*/
+        if (divN % 3 == 0) {
+
+            /* 설명. 3으로 나눠진다면, 5의 갯수와 3의 개수를 count에 대입 */
+            count = num1Count + (divN / 3);
+        }
+
+        /* 설명. 반환되는 값은 조건문 성립 여부에 따라 갈린다. */
+        return count;
+    }
+
+
+}
---
+++ chap17-algorithm-lecture-source/src/main/java/com/ohgiraffers/chap05/section01/tree/Application1.java
@@ -0,0 +1,78 @@
+package com.ohgiraffers.chap05.section01.tree;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Application1 {
+
+    static int N;
+    static int[] parent;
+    static boolean[] isVisit;
+    static StringTokenizer st;
+
+    /* 설명. List<Integer>[]로 구현한 이유
+     *       하나의 트리에 대해 여러 자식을 가질 수 있도록 구현하기 위해.
+     * */
+    static List<Integer>[] list;
+    static StringBuilder sb;
+
+    public static BufferedReader toBufferedReader(String str) {
+        InputStream is = new ByteArrayInputStream(str.getBytes());
+        return new BufferedReader(new InputStreamReader(is));
+    }
+
+    public static String solution(String input) throws IOException {
+        BufferedReader br = toBufferedReader(input);
+        sb.delete(0, sb.length());
+
+        N = Integer.valueOf(br.readLine());
+
+        /* 설명. 노드의 번호와 인덱스 번호를 일치시켜주기 위해 N+1로 배열을 구성 */
+        isVisit = new boolean[N + 1];
+        list = new ArrayList[N + 1];
+        parent = new int[N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.valueOf(st.nextToken());
+            int b = Integer.valueOf(st.nextToken());
+
+            /* 설명. 트리는 양방향이기 때문이다. */
+            list[a].add(b);
+            list[b].add(a);
+
+        }
+
+        dfs(1);
+
+        for (int i = 2; i < parent.length; i++) {
+            sb.append(parent[i]);
+            sb.append(" ");
+
+        }
+
+
+        return sb.toString();
+    }
+
+    private static void dfs(int parentNode) {
+        /* 설명. 이미 방문한 노드들은 부모 노드들이고, 부모 노드들을 재방문 하지 않도록 한다. (방문 배열의 의미)*/
+        isVisit[parentNode] = true;
+
+        /* 설명. 입력받은 값을 통해 생성된 list로부터 관련있는 노드들을 확인하기 위한 반복문 */
+        for (int node : list[parentNode]) {
+
+            /* 설명. 방문한 적이 없다면 자식노드이다. (루트부터 방문을 체크하며 연관된 것들을 따라 내려 왔기 때문) */
+            if (!isVisit[node]) {
+                parent[node] = parentNode;  // 이 줄부터는 사실상 node가 childnode를 의미
+                dfs(node);
+            }
+        }
+    }
+}
